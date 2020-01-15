466b479a27ebb268690e778402d8f295e93db47b
Ross Malcolm <rossmalc75@gmail.com>
2020-01-14 20:35:53 -0500

    Initial Commit

---
+++ A1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A1
+//
+//  Created by Ross Malcolm on 14-01-2020.
+//  Copyright © 2020 Ross Malcolm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
